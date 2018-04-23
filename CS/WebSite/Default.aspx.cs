1131fe4f2aba1f1303e5226cb83c4638c8e96ba3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,23 @@
+using System;
+using DevExpress.Web.ASPxGridView;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void grid_CustomSummaryCalculate(object sender, DevExpress.Data.CustomSummaryEventArgs e) {
+        ASPxSummaryItem summary = e.Item as ASPxSummaryItem;
+        if (summary.FieldName != "UnitsInStock")
+            return;
+
+        if (e.IsTotalSummary) {
+            int totalValue = 0;
+            foreach (object value in grid.GetSelectedFieldValues("UnitsInStock")) {
+                totalValue += Convert.ToInt32(value);
+            }
+            e.TotalValue = totalValue;
+            e.TotalValueReady = true;
+        }
+    }
+    protected void grid_SelectionChanged(object sender, EventArgs e) {
+        grid.DataBind();
+    }
+}
